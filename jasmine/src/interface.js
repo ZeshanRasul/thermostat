1372f6612127fd6b16a6016dbbbce28ab528b277
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-18 18:43:34 +0000

    added API and search function for city weather and temperature

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -1,7 +1,8 @@
-
-$(document).ready(function() {
+$( document ).ready(function() {
 
   var thermostat = new Thermostat();
+  var city, cityName, cityTemp, cityWeather;
+
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
@@ -48,4 +49,29 @@ $(document).ready(function() {
     updatePowerStatus();
   });
 
-});
+  $('#select-city').on("submit", function( event ){
+    event.preventDefault();
+    city = $( this ).serialize() ;
+    $.get('http://api.openweathermap.org/data/2.5/forecast/weather?' + city + '&APPID=ca6f6c4482e27abd7c68f8d5e303960f&units=metric', function ( report ) {
+      console.log(report);
+      console.log(report.city.name);
+      console.log(report.list[0].main.temp);
+      console.log(report.list[0].weather[0].description);
+      cityName = (report.city.name);
+      cityTemp = (report.list[0].main.temp);
+      cityWeather = (report.list[0].weather[0].description);
+      $('#city-name').text(cityName);
+      $('#country-temperature').text(cityTemp);
+      $('#country-weather').text(cityWeather);
+
+      });
+    });
+
+  });
+
+  // $('#select-city').submit(function(event){
+  //   event.preventDefault();
+  //   var city = $('#current-city').val();
+  //   $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  //     $('#country-temperature').text(data.main.temp);
+  //   });
