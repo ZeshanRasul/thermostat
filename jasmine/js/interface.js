ca9c7cdf30c715ce6b98f9e61b8f1170ca7a0698
ZeshanRasul <zeshanrasul90@gmail.com>
2016-02-21 03:01:01 +0000

    Implemented google maps API and added further styling and images

--- jasmine/js/interface.js
+++ jasmine/js/interface.js
@@ -1,30 +1,45 @@
 $( document ).ready(function() {
-
   var thermostat = new Thermostat();
-  var city, cityName, cityTemp, cityWeather, cityWind;
-
-
+  var city, cityName, cityTemp, cityWeather, cityWind, lat, long;
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
     $('#section_1').attr('class', thermostat.energyUse());
   }
+
   function updatePowerStatus() {
     if (thermostat.isPowerSaving === true){
-      $('#power-saving-status').text('on');
+      $('#power-saving-status').text('On');
     } else if (thermostat.isPowerSaving === false) {
-      $('#power-saving-status').text('off');
+      $('#power-saving-status').text('Off');
     }
   }
 
-  // function windLevel() {
-  //   if (cityWind > 3) {
-  //     $('#wind_pic').attr('class', "windy");
-  //   else {
-  //     $('#wind_pic').attr('class', "not_windy");
-  //   }
-  //   }
-  // }
+  function windLevel(cityWind) {
+    if (cityWind > 10) {
+      $('#wind_pic').attr('class', "windy");
+    } else {
+      $('#wind_pic').attr('class', "not_windy");
+    }
+  }
+
+  function warmthLevel(cityTemp) {
+    if (cityTemp > 18) {
+      $('#weather_pic').attr('class', "hot");
+    } else {
+      $('#weather_pic').attr('class', "cold");
+    }
+  }
+
+  function initialize(lat, long) {
+    var mapProp = {
+      center:new google.maps.LatLng(lat,long),
+      zoom: 10,
+      mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
 
   $('#temperature').text(thermostat.temp);
   updateTemperature();
@@ -42,7 +57,6 @@ $( document ).ready(function() {
   $('#reset-temperature').click(function(){
     thermostat.reset();
     updateTemperature();
-
   });
 
   $('#power-save-on').click(function(){
@@ -68,60 +82,18 @@ $( document ).ready(function() {
       cityWind = report.list[0].wind.speed;
       cityTemp = report.list[0].main.temp;
       cityWeather = report.list[0].weather[0].description;
+      lat = report.city.coord.lat
+      long = report.city.coord.lon
+      console.log(lat);
+      console.log(long);
+      windLevel(cityWind);
+      warmthLevel(cityTemp);
       $('#city-name').text(cityName);
       $('#city-temperature').text(cityTemp);
       $('#city-wind').text(cityWind);
       $('#city-weather').text(cityWeather);
-      function windLevel(cityWind) {
-        if (cityWind > 3) {
-          $('#wind_pic').attr('class', "windy");
-        } else {
-          $('#wind_pic').attr('class', "not_windy");
-        }
-      }
-      windLevel(cityWind);
-
-
+      initialize(lat, long);
     });
-
-
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // $('#select-city').on("submit", function( event ){
-  //   event.preventDefault();
-  //   city = $( this ).serialize() ;
-  //   $.get('http://api.openweathermap.org/data/2.5/forecast/weather?' + city + '&APPID=ca6f6c4482e27abd7c68f8d5e303960f&units=metric', function ( report ) {
-  //     console.log(report);
-  //     console.log(report.city.name);
-  //     console.log(report.list[0].main.temp);
-  //     console.log(report.list[0].weather[0].description);
-  //     cityName = (report.city.name);
-  //     cityTemp = (report.list[0].main.temp);
-  //     cityWeather = (report.list[0].weather[0].description);
-  //     $('#city-name').text(cityName);
-  //     $('#country-temperature').text(cityTemp);
-  //     $('#country-weather').text(cityWeather);
-
-      // });
-
+  
   });
